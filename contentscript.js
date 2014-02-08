e055b700f48e50dbcf70b352b6482b0181fa80ed
Adi <adir@yahoo-inc.com>
2014-02-08 11:55:14 +0530

    changed UI and date time format

--- contentscript.js
+++ contentscript.js
@@ -7,6 +7,6 @@ chrome.extension.onMessage.addListener(function (message, sender, sendResponse) 
     //this will fire when asked for info by the popup
     //alert("message received from tab" + message);
     var d=new Date(lastmodified),
-        datetime=d.toUTCString();
+        datetime=d.toTimeString();
     sendResponse({"url": url, "lastmodified": datetime});
 });
